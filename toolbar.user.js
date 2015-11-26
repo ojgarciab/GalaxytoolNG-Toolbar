0e6d914eda844c52574d15fd83492d802dafcf4d
Oscar García <ojgarciab@users.noreply.github.com>
2015-11-26 13:16:43 +0100

    Update toolbar.user.js

--- toolbar.user.js
+++ toolbar.user.js
@@ -2,7 +2,7 @@
 // @name        GalaxytoolNG Toolbar
 // @namespace   https://foro.gt.linaresdigital.com
 // @description Galaxytool Toolbar compabible with Ogame 6
-// @version     0.2.1
+// @version     0.2.2
 // @author      Óscar Javier García Baudet
 // @namespace   https://github.com/GalaxytoolNG
 // @downloadURL https://raw.githubusercontent.com/GalaxytoolNG/GalaxytoolNG-Toolbar/master/toolbar.user.js
