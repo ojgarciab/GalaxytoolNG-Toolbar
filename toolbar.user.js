3396db7cea52168fbd5b2530fa4c269fc87dc168
Oscar García <ojgarciab@users.noreply.github.com>
2016-03-26 10:19:27 +0100

    Fix #9: Change console.log to GM_log

--- toolbar.user.js
+++ toolbar.user.js
@@ -2,7 +2,7 @@
 // @name        GalaxytoolNG Toolbar
 // @namespace   https://foro.gt.linaresdigital.com
 // @description Galaxytool Toolbar compatible with Ogame 6
-// @version     0.3.8
+// @version     0.3.9
 // @author      Óscar Javier García Baudet
 // @namespace   https://github.com/GalaxytoolNG
 // @downloadURL https://raw.githubusercontent.com/GalaxytoolNG/GalaxytoolNG-Toolbar/master/toolbar.user.js
@@ -13,7 +13,7 @@
 // @copyright   2015+, Óscar Javier García Baudet
 // ==/UserScript==
 
-/* jshint browser:true, devel: true, newcap: false */
+/* jshint browser:true, newcap: false */
 /* jshint -W097 */
 /* Fix: JSHint doesn't know about greasemonkey environment as eslint */
 /* global GM_xmlhttpRequest:false, GM_log:false */
@@ -28,29 +28,29 @@
 
     var observer = new MutationObserver(function(mutations) {  
         mutations.forEach(function(mutation) {
-            console.log('---------[ MUTATION EVENT STARTS HERE ]-------------');
-            console.log(mutation);
+            GM_log('---------[ MUTATION EVENT STARTS HERE ]-------------');
+            GM_log(mutation);
             if (mutation.target.className != 'ui-tabs-panel ui-widget-content ui-corner-bottom') {
                 return;
             }
-            console.log('---------[ MUTATION EVENT WILL BE PROCESSED ]-------------');
+            GM_log('---------[ MUTATION EVENT WILL BE PROCESSED ]-------------');
             var apiList = [];
             for (var i = 0; i < mutation.addedNodes.length; ++i) {
                 var xpathResult = document.evaluate(".//li[@data-msg-id]", mutation.addedNodes.item(i), null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null );
                 var thisNode = xpathResult.iterateNext();
                 while (thisNode) {
-                    console.log('---------[ XPATH NODE ]-------------');
+                    GM_log('---------[ XPATH NODE ]-------------');
                     var apiKey = document.evaluate(".//a[starts-with(@href,'ogame-api://')]", thisNode, null, XPathResult.FIRST_ORDERED_NODE_TYPE , null );
                     if (apiKey.singleNodeValue !== null && apiKey.singleNodeValue.getAttribute('href') !== null) {
-                        console.log('API key found: ' + apiKey.singleNodeValue.getAttribute('href'));
+                        GM_log('API key found: ' + apiKey.singleNodeValue.getAttribute('href'));
                         apiList.push(apiKey.singleNodeValue.getAttribute('href'));
                     } else {
-                        console.log('API key not found in this message');
+                        GM_log('API key not found in this message');
                     }
                     thisNode = xpathResult.iterateNext();
                 }
             }
-            console.log('---------[ MUTATION EVENT ENDS HERE ]-------------');
+            GM_log('---------[ MUTATION EVENT ENDS HERE ]-------------');
             /* Set testing URL with localStorage.getItem('Galaxytoolng_url', 'http://...'); in console */
             if (localStorage.getItem('Galaxytoolng_url') === false) {
               return;
