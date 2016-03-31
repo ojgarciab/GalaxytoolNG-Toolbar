7230b6b1e47703cc6b5db6701c0c21c1e3a1fd42
Oscar García <ojgarciab@users.noreply.github.com>
2016-03-31 22:51:52 +0200

    Code refactoring and page detection

--- toolbar.user.js
+++ toolbar.user.js
@@ -2,7 +2,7 @@
 // @name        GalaxytoolNG Toolbar: Messages
 // @namespace   https://foro.gt.linaresdigital.com
 // @description Galaxytool Toolbar compatible with Ogame 6
-// @version     0.4.4
+// @version     0.4.5
 // @author      Óscar Javier García Baudet
 // @namespace   https://github.com/GalaxytoolNG
 // @downloadURL https://raw.githubusercontent.com/GalaxytoolNG/GalaxytoolNG-Toolbar/master/toolbar.user.js
@@ -38,12 +38,8 @@
         return string;
     }
 
-    /* Get page */
-    var page = window.location.search.match(/[?&]page=[^&]*/).replace(/[?&]page=/, '');
-    GM_log("page: " + page);
-    var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
-    var base = document.querySelector('#buttonz > div.content');
-    var menuTableTools = document.getElementById('menuTableTools');
+    /* Menu */
+        var menuTableTools = document.getElementById('menuTableTools');
     var menuGTNG = document.createElement('li');
     function toogleMenu() {
         var bottonz = document.getElementById('buttonz');
@@ -59,49 +55,60 @@
     },false);
     menuTableTools.appendChild(menuGTNG);
 
-    var observer = new MutationObserver(function(mutations) {  
-        mutations.forEach(function(mutation) {
-            GM_log('---------[ MUTATION EVENT STARTS HERE ]-------------');
-            GM_log(mutation);
-            if (mutation.target.className != 'ui-tabs-panel ui-widget-content ui-corner-bottom') {
-                return;
-            }
-            GM_log('---------[ MUTATION EVENT WILL BE PROCESSED ]-------------');
-            var apiList = [];
-            for (var i = 0; i < mutation.addedNodes.length; ++i) {
-                var xpathResult = document.evaluate(".//li[@data-msg-id]", mutation.addedNodes.item(i), null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null );
-                var thisNode = xpathResult.iterateNext();
-                while (thisNode) {
-                    GM_log('---------[ XPATH NODE ]-------------');
-                    var apiKey = document.evaluate(".//a[starts-with(@href,'ogame-api://')]", thisNode, null, XPathResult.FIRST_ORDERED_NODE_TYPE , null );
-                    if (apiKey.singleNodeValue !== null && apiKey.singleNodeValue.getAttribute('href') !== null) {
-                        GM_log('API key found: ' + apiKey.singleNodeValue.getAttribute('href'));
-                        apiList.push(apiKey.singleNodeValue.getAttribute('href'));
-                    } else {
-                        GM_log('API key not found in this message');
+    /* Get page */
+    var page = window.location.search.match(/[?&]page=([^&]*)/)[1];
+    GM_log("Page detected: " + page);
+    switch(page) {
+        case 'messages':
+            var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
+            var base = document.querySelector('#buttonz > div.content');
+            var observer = new MutationObserver(function(mutations) {  
+                mutations.forEach(function(mutation) {
+                    GM_log('---------[ MUTATION EVENT STARTS HERE ]-------------');
+                    GM_log(mutation);
+                    if (mutation.target.className != 'ui-tabs-panel ui-widget-content ui-corner-bottom') {
+                        return;
                     }
-                    thisNode = xpathResult.iterateNext();
-                }
-            }
-            GM_log('---------[ MUTATION EVENT ENDS HERE ]-------------');
-            /* Set testing URL with localStorage.getItem('Galaxytoolng_url', 'http://...'); in console */
-            if (localStorage.getItem('Galaxytoolng_url') === false) {
-              return;
-            }
-            /* Send results to every destination configured */
-            GM_xmlhttpRequest({
-                method: "POST",
-                url: localStorage.getItem('Galaxytoolng_url'),
-                data: apiList,
-                headers: {
-                    "Content-Type": "application/x-www-form-urlencoded"
-                },
-                onload: function(response) { GM_log(response.responseText); }
+                    GM_log('---------[ MUTATION EVENT WILL BE PROCESSED ]-------------');
+                    var apiList = [];
+                    for (var i = 0; i < mutation.addedNodes.length; ++i) {
+                        var xpathResult = document.evaluate(".//li[@data-msg-id]", mutation.addedNodes.item(i), null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null );
+                        var thisNode = xpathResult.iterateNext();
+                        while (thisNode) {
+                            GM_log('---------[ XPATH NODE ]-------------');
+                            var apiKey = document.evaluate(".//a[starts-with(@href,'ogame-api://')]", thisNode, null, XPathResult.FIRST_ORDERED_NODE_TYPE , null );
+                            if (apiKey.singleNodeValue !== null && apiKey.singleNodeValue.getAttribute('href') !== null) {
+                                GM_log('API key found: ' + apiKey.singleNodeValue.getAttribute('href'));
+                                apiList.push(apiKey.singleNodeValue.getAttribute('href'));
+                            } else {
+                                GM_log('API key not found in this message');
+                            }
+                            thisNode = xpathResult.iterateNext();
+                        }
+                    }
+                    GM_log('---------[ MUTATION EVENT ENDS HERE ]-------------');
+                    /* Set testing URL with localStorage.getItem('Galaxytoolng_url', 'http://...'); in console */
+                    if (localStorage.getItem('Galaxytoolng_url') === false) {
+                        return;
+                    }
+                    /* Send results to every destination configured */
+                    GM_xmlhttpRequest({
+                        method: "POST",
+                        url: localStorage.getItem('Galaxytoolng_url'),
+                        data: apiList,
+                        headers: {
+                            "Content-Type": "application/x-www-form-urlencoded"
+                        },
+                        onload: function(response) { GM_log(response.responseText); }
+                    });
+                });
             });
-        });
-    });
-    observer.observe(base, {
-        subtree: true,
-        childList: true
-    });
+            observer.observe(base, {
+                subtree: true,
+                childList: true
+            });
+            break;
+        default:
+            GM_log('Nothing to do here');
+    }
  })();
